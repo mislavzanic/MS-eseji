60fd06f3f6f23820cac2d88c88d3a1661d12fab9
Mislav Zanic <mislav.zanic@photomath.com>
2022-04-17 09:38:23 +0200

    Minor fix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    python310Packages.numpy
-    python310Packages.matplotlib
-    python310Packages.pandas
-    python310Packages.scipy
-    python310Packages.ipython
-    python310Packages.requests
-    python310Packages.z3
-    python310Packages.notebook
+    python39
+    python39Packages.numpy
+    python39Packages.matplotlib
+    python39Packages.pandas
+    python39Packages.scipy
+    python39Packages.ipython
+    python39Packages.requests
+    python39Packages.z3
+    python39Packages.notebook
   ];
   shellHook = ''
     jupyter notebook
